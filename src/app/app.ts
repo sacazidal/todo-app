99affc98de4e590726f043a98b01c33e0ac9709f
darkness <tw1nkle001@yandex.ru>
2025-11-27 00:31:30 +0700

    feat(ui): build initial ap layout with responsive structure and components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from './components/button/button';
+import { InputComponent } from './components/input/input';
+import { TaskComponent } from './components/task/task';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ButtonComponent, InputComponent, TaskComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
