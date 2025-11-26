4ee6bc4abd0f569ed1c4d325063b9677c544a698
darkness <tw1nkle001@yandex.ru>
2025-11-27 00:42:41 +0700

    feat(app): connected TaskService to UI with reactive rendering and task lifecycle handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonComponent } from './components/button/button';
 import { InputComponent } from './components/input/input';
 import { TaskComponent } from './components/task/task';
+import { TaskService } from './services/task';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,31 @@ import { TaskComponent } from './components/task/task';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  private taskService = inject(TaskService);
+
+  protected tasks = this.taskService.tasks$;
+  protected stats = this.taskService.getStats();
+
+  protected newTaskText = '';
+
+  addTask() {
+    this.taskService.addTask(this.newTaskText);
+    this.newTaskText = '';
+    this.updateStats();
+  }
+
+  toggleTask(id: number) {
+    this.taskService.toggleCompleted(id);
+    this.updateStats();
+  }
+
+  deleteTask(id: number) {
+    this.taskService.deleteTask(id);
+    this.updateStats();
+  }
+
+  private updateStats() {
+    this.stats = { ...this.taskService.getStats() };
+  }
+}
